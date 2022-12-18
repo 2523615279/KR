648414f934a1474f3df57f460e226cc3bbd846f0
ling <2523615279@qq.com>
2022-12-18 20:46:57 +0800

    农场添加代理

--- jd_fruit_task.js
+++ jd_fruit_task.js
@@ -24,6 +24,8 @@ cron "5 6-18/6 * * *" script-path=jd_fruit_task.js,tag=东东农场日常任务
 export DO_TEN_WATER_AGAIN="" 默认再次浇水
 
 */
+require("global-agent/bootstrap");
+global.GLOBAL_AGENT.HTTP_PROXY="http://140.238.85.247:8000";
 const $ = new Env('东东农场日常任务');
 let cookiesArr = [],
     cookie = '',
